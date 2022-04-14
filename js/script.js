d095df27fd7fba6db37e8808cd0071d107914a1c
Andrey Kudinov <2499931@gmail.com>
2022-04-14 21:37:10 +0400

    Fix arrows

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
   $('.carousel__inner').slick({
     speed: 1200,
     // adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="/img/logo/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="/img/logo/right.svg"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="./img/logo/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="./img/logo/right.svg"></button>',
     responsive: {
       breakpoint: 768,
       settings: {
